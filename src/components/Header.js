5c592529c5e0af2254a794101048fa7203d97bcd
Ishan <ishan@thinksys.com>
2023-01-22 19:05:33 +0530

    day6

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,42 @@
-const Title = () =>{
-    return (
-    <a href="/">
-    <img className="logo" 
-    alt="logo" 
-    src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj" />
-    </a>
-    );
-    };
+import { useState } from "react";
 
-    //React Component
-const Header =()=>{ 
-    return (
+const Title = () => {
+  return (
+    <a href="/">
+      <img
+        className="logo"
+        alt="logo"
+        src="https://yt3.ggpht.com/ytc/AMLnZu_EC-ECXAxRAixWGEfMsE1rdSoetBHyxmLNdtCB=s900-c-k-c0x00ffffff-no-rj"
+      />
+    </a>
+  );
+};
+
+// const loggedInUser = () => {
+//   return true;
+// };
+//React Component
+const Header = () => {
+  const [isLoggedIn, setisLoggedIn] = useState(true);
+  return (
     <div className="header">
-    <Title/>
-    <div className="nav-items">
+      <Title />
+      <div className="nav-items">
         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Contact</li>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Contact</li>
         </ul>
+      </div>
+      {isLoggedIn ? (
+        <button onClick={() => {setisLoggedIn(false);}}>
+          Logout
+        </button>
+      ) : (
+        <button onClick={()=>{setisLoggedIn(true)}}>Login</button>
+      )}
     </div>
-    </div>
-    );
-    };
+  );
+};
 export default Header;
